e3b870c497a54771dd56340c8df9704ccbb487fd
SANDrex14 <sandrochapidze990@gmail.com>
2025-01-24 07:51:40 -0800

    commit

---
+++ Goa Group 41/day 93/homework/leson1.py
@@ -0,0 +1,9 @@
+# 1
+# mkdir transport_recommendation
+# cd transport_recommendation
+
+# 2
+# git init
+
+# 3
+# touch transport.py
---
+++ Goa Group 41/day 93/homework/leson3.py
@@ -0,0 +1,8 @@
+# 1
+# git add transport.py
+
+# 2
+# git commit -m "commit"
+
+# 3
+# git remote add origin <რეპოზიტორიის URL>
